8e745fcd3b2f269921bc7cb851955b299bf8a8e0
kathanon <cower@cower.se>
2022-09-17 12:15:19 +0200

    Added support for `vanishIfMouseDistant = true`, and polished for release.

--- Source/FloatSubMenu.cs
+++ Source/FloatSubMenu.cs
@@ -7,19 +7,15 @@ using System.Reflection;
 using UnityEngine;
 using Verse;
 
-namespace CraftWithColor
-{
-    public class FloatSubMenu : FloatMenuOption
-    {
+namespace FloatSubMenus {
+    public class FloatSubMenu : FloatMenuOption {
         private readonly List<FloatMenuOption> subOptions;
-        private readonly string subTitle;
         private readonly float extraPartWidthOuter;
         private readonly Func<Rect, bool> extraPartOnGUIOuter;
 
         private FloatSubMenuInner subMenu = null;
         private Action parentCloseCallback = null;
         private bool parentSetUp = false;
-        private bool open = false;
         private bool subMenuOptionChosen = false;
         private Rect extraGUIRect = new Rect(-1f, -1f, 0f, 0f);
 
@@ -29,31 +25,25 @@ namespace CraftWithColor
         private const float ArrowOffset = 4f;
         private const float ArrowAlpha = 0.6f;
 
-        public FloatSubMenu(string label, List<FloatMenuOption> subOptions, string subTitle = null, MenuOptionPriority priority = MenuOptionPriority.Default, Thing revalidateClickTarget = null, float extraPartWidth = 0, Func<Rect, bool> extraPartOnGUI = null, WorldObject revalidateWorldClickTarget = null, bool playSelectionSound = true, int orderInPriority = 0) 
-            : base(label, NoAction, priority, null, revalidateClickTarget, extraPartWidth + ArrowExtraWidth, null, revalidateWorldClickTarget, playSelectionSound, orderInPriority)
-        {
+        public FloatSubMenu(string label, List<FloatMenuOption> subOptions, MenuOptionPriority priority = MenuOptionPriority.Default, Thing revalidateClickTarget = null, float extraPartWidth = 0, Func<Rect, bool> extraPartOnGUI = null, WorldObject revalidateWorldClickTarget = null, bool playSelectionSound = true, int orderInPriority = 0)
+            : base(label, NoAction, priority, null, revalidateClickTarget, extraPartWidth + ArrowExtraWidth, null, revalidateWorldClickTarget, playSelectionSound, orderInPriority) {
             this.subOptions = subOptions;
-            this.subTitle = subTitle;
             extraPartOnGUIOuter = extraPartOnGUI;
             extraPartWidthOuter = extraPartWidth;
             this.extraPartOnGUI = DrawExtra;
         }
 
-        public FloatSubMenu(string label, List<FloatMenuOption> subOptions, ThingDef shownItemForIcon, string subTitle = null, MenuOptionPriority priority = MenuOptionPriority.Default, Thing revalidateClickTarget = null, float extraPartWidth = 0, Func<Rect, bool> extraPartOnGUI = null, WorldObject revalidateWorldClickTarget = null, bool playSelectionSound = true, int orderInPriority = 0) 
-            : base(label, NoAction, shownItemForIcon, priority, null, revalidateClickTarget, extraPartWidth + ArrowExtraWidth, null, revalidateWorldClickTarget, playSelectionSound, orderInPriority)
-        {
+        public FloatSubMenu(string label, List<FloatMenuOption> subOptions, ThingDef shownItemForIcon, MenuOptionPriority priority = MenuOptionPriority.Default, Thing revalidateClickTarget = null, float extraPartWidth = 0, Func<Rect, bool> extraPartOnGUI = null, WorldObject revalidateWorldClickTarget = null, bool playSelectionSound = true, int orderInPriority = 0)
+            : base(label, NoAction, shownItemForIcon, priority, null, revalidateClickTarget, extraPartWidth + ArrowExtraWidth, null, revalidateWorldClickTarget, playSelectionSound, orderInPriority) {
             this.subOptions = subOptions;
-            this.subTitle = subTitle;
             extraPartOnGUIOuter = extraPartOnGUI;
             extraPartWidthOuter = extraPartWidth;
             this.extraPartOnGUI = DrawExtra;
         }
 
-        public FloatSubMenu(string label, List<FloatMenuOption> subOptions, Texture2D itemIcon, Color iconColor, string subTitle = null, MenuOptionPriority priority = MenuOptionPriority.Default, Thing revalidateClickTarget = null, float extraPartWidth = 0, Func<Rect, bool> extraPartOnGUI = null, WorldObject revalidateWorldClickTarget = null, bool playSelectionSound = true, int orderInPriority = 0) 
-            : base(label, NoAction, itemIcon, iconColor, priority, null, revalidateClickTarget, extraPartWidth + ArrowExtraWidth, null, revalidateWorldClickTarget, playSelectionSound, orderInPriority)
-        {
+        public FloatSubMenu(string label, List<FloatMenuOption> subOptions, Texture2D itemIcon, Color iconColor, MenuOptionPriority priority = MenuOptionPriority.Default, Thing revalidateClickTarget = null, float extraPartWidth = 0, Func<Rect, bool> extraPartOnGUI = null, WorldObject revalidateWorldClickTarget = null, bool playSelectionSound = true, int orderInPriority = 0)
+            : base(label, NoAction, itemIcon, iconColor, priority, null, revalidateClickTarget, extraPartWidth + ArrowExtraWidth, null, revalidateWorldClickTarget, playSelectionSound, orderInPriority) {
             this.subOptions = subOptions;
-            this.subTitle = subTitle;
             extraPartOnGUIOuter = extraPartOnGUI;
             extraPartWidthOuter = extraPartWidth;
             this.extraPartOnGUI = DrawExtra;
@@ -61,15 +51,13 @@ namespace CraftWithColor
 
         private static void NoAction() { }
 
-        public bool DrawExtra(Rect rect)
-        {
+        public bool DrawExtra(Rect rect) {
             extraGUIRect = rect.RightPartPixels(ArrowExtraWidth);
             extraPartOnGUIOuter?.Invoke(rect.LeftPartPixels(extraPartWidthOuter));
             return false;
         }
 
-        private static void DrawArrow(Rect rect)
-        {
+        private static void DrawArrow(Rect rect) {
             rect.width -= ArrowOffset;
 
             GameFont font = Text.Font;
@@ -86,15 +74,13 @@ namespace CraftWithColor
             GUI.color = color;
         }
 
-        public override bool DoGUI(Rect rect, bool colonistOrdering, FloatMenu floatMenu)
-        {
+        public override bool DoGUI(Rect rect, bool colonistOrdering, FloatMenu floatMenu) {
             SetupParent(floatMenu);
 
             MouseArea mouseArea = FindMouseArea(rect, floatMenu);
             bool inExtraSpace = Mouse.IsOver(extraGUIRect);
-            if (mouseArea == (open ? MouseArea.Menu : MouseArea.Option))
-            {
-                MouseAction(rect, !open);
+            if (mouseArea == (Open ? MouseArea.Menu : MouseArea.Option)) {
+                MouseAction(rect, !Open, floatMenu);
             }
 
             // When the sub menu is open, let super implementation know only about
@@ -102,8 +88,7 @@ namespace CraftWithColor
             // mouse is in our extraPartOnGUI space, since it does not highlight
             // option then.
             Vector2 mouse = Event.current.mousePosition;
-            if ((open && mouseArea == MouseArea.Outside) || inExtraSpace)
-            {
+            if ((Open && mouseArea == MouseArea.Outside) || inExtraSpace) {
                 Event.current.mousePosition = new Vector2(rect.x + 2f, rect.y + 2f);
             }
 
@@ -112,23 +97,32 @@ namespace CraftWithColor
 
             // Reset mouse position
             Event.current.mousePosition = mouse;
-            
+
             if (subMenuOptionChosen) { floatMenu.PreOptionChosen(this); }
             return subMenuOptionChosen;
         }
 
-        private void SetupParent(FloatMenu parent)
-        {
-            if (!(parentSetUp || parent is FloatSubMenuInner))
-            {
+        public bool Open => subMenu != null && subMenu.IsOpen;
+
+        internal static bool ShouldReplaceDistanceFor(FloatMenu menu, ref float distance) {
+            var set = OpenMenuSet.For(menu);
+            if (set != null) {
+                distance = set.MinDistance;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        private void SetupParent(FloatMenu parent) {
+            if (!(parentSetUp || parent is FloatSubMenuInner)) {
                 parentSetUp = true;
                 parentCloseCallback = parent.onCloseCallback;
                 parent.onCloseCallback = OnParentClose;
             }
         }
 
-        private void OnParentClose()
-        {
+        private void OnParentClose() {
             CloseSubMenu();
             // Call any previously set action
             parentCloseCallback?.Invoke();
@@ -136,8 +130,7 @@ namespace CraftWithColor
 
         private enum MouseArea { Option, Menu, Outside }
 
-        private MouseArea FindMouseArea(Rect option, FloatMenu menu)
-        {
+        private MouseArea FindMouseArea(Rect option, FloatMenu menu) {
             option.height--;
             if (Mouse.IsOver(option)) { return MouseArea.Option; }
 
@@ -145,79 +138,144 @@ namespace CraftWithColor
             return Mouse.IsOver(menu.windowRect.AtZero()) ? MouseArea.Menu : MouseArea.Outside;
         }
 
-        private void MouseAction(Rect rect, bool enter)
-        {
-            if (enter)
-            {
+        private void MouseAction(Rect rect, bool enter, FloatMenu parentMenu) {
+            if (enter) {
                 Vector2 localPos = new Vector2(rect.xMax, rect.yMin) + MenuOffset;
-                OpenSubMenu(localPos);
-            }
-            else
-            {
+                OpenSubMenu(parentMenu, localPos);
+            } else {
                 CloseSubMenu();
             }
         }
 
-        private void OpenSubMenu(Vector2 localPos)
-        {
-            if (!open)
-            {
-                open = true;
+        private void OpenSubMenu(FloatMenu parentMenu, Vector2 localPos) {
+            if (!Open) {
                 Vector2 mouse = Event.current.mousePosition;
                 Vector2 offset = localPos - mouse;
                 SoundDef sound = SoundDefOf.FloatMenu_Open;
                 SoundDefOf.FloatMenu_Open = null;
-                subMenu = new FloatSubMenuInner(this, subOptions, subTitle, offset);
+                subMenu = new FloatSubMenuInner(
+                    this,
+                    subOptions,
+                    offset,
+                    parentMenu.vanishIfMouseDistant);
                 SoundDefOf.FloatMenu_Open = sound;
                 Find.WindowStack.Add(subMenu);
+                OpenMenuSet.Open(parentMenu, subMenu);
             }
         }
 
-        private void CloseSubMenu()
-        {
-            if (open)
-            {
-                open = false;
+        private void CloseSubMenu() {
+            if (Open) {
                 Find.WindowStack.TryRemove(subMenu, doCloseSound: false);
                 subMenu = null;
             }
         }
 
-        private class FloatSubMenuInner : FloatMenu
-        {
+        private class OpenMenuSet {
+            private static readonly Dictionary<FloatMenu,OpenMenuSet> sets =
+                new Dictionary<FloatMenu, OpenMenuSet>();
+
+            private readonly List<FloatMenu> menus = new List<FloatMenu>();
+            private readonly List<Rect> rects = new List<Rect>();
+
+            private bool cacheValid = false;
+            private Vector2 cachedPosition;
+            private float cachedDistance;
+
+            private OpenMenuSet(FloatMenu parent, FloatSubMenuInner child) {
+                Add(parent);
+                Add(child);
+            }
+
+            private void Add(FloatMenu menu) {
+                if (!menus.Contains(menu)) {
+                    menus.Add(menu);
+                    rects.Add(menu.windowRect.ContractedBy(-5f));
+                }
+                sets[menu] = this;
+                cacheValid = false;
+            }
+
+            private void Remove(int i) {
+                sets.Remove(menus[i]);
+                menus.RemoveAt(i);
+                rects.RemoveAt(i);
+                cacheValid = false;
+            }
+
+            private void Remove(FloatMenu menu) {
+                int i = menus.IndexOf(menu);
+                if (i > 0) {
+                    Remove(i);
+                }
+                // If only the vanilla menu is left, remove it as well.
+                // If the menu to remove is the vanilla one, remove all.
+                if (menus.Count == 1 || i == 0) {
+                    for (int j = menus.Count - 1; j >= 0; j--) {
+                        Remove(j);
+                    }
+                }
+            }
+
+            public static OpenMenuSet For(FloatMenu menu) =>
+                sets.TryGetValue(menu, out var set) ? set : null;
+
+            public static void Open(FloatMenu parent, FloatSubMenuInner child) {
+                if (sets.TryGetValue(parent, out var set)) {
+                    set.Add(child);
+                } else {
+                    new OpenMenuSet(parent, child);
+                }
+                var list = sets[parent].menus.Select(m => m.ID).ToStringSafeEnumerable();
+            }
+
+            public static void Close(FloatMenu menu) {
+                if (sets.TryGetValue(menu, out var set)) {
+                    set.Remove(menu);
+                }
+            }
+
+            public float MinDistance {
+                get {
+                    var pos = UI.MousePositionOnUIInverted;
+                    if (!cacheValid || pos != cachedPosition) {
+                        cacheValid = true;
+                        cachedPosition = pos;
+                        cachedDistance = rects.Min(r => GenUI.DistFromRect(r, pos));
+                    }
+                    return cachedDistance;
+                }
+            }
+        }
+
+        private class FloatSubMenuInner : FloatMenu {
             public Vector2 MouseOffset;
             public FloatSubMenu parent;
 
-            public FloatSubMenuInner(FloatSubMenu parent, List<FloatMenuOption> options, string title, Vector2 MouseOffset) : base(options, title, false) 
-            {
+            public FloatSubMenuInner(FloatSubMenu parent, List<FloatMenuOption> options, Vector2 MouseOffset, bool vanish) : base(options) {
                 this.MouseOffset = MouseOffset;
                 this.parent = parent;
-
-                // TODO: support vanishIfMouseDistant = true
-                vanishIfMouseDistant = false;
                 onlyOneOfTypeAllowed = false;
+                vanishIfMouseDistant = vanish;
             }
 
-            protected override void SetInitialSizeAndPosition()
-            {
+            protected override void SetInitialSizeAndPosition() {
                 Vector2 pos = UI.MousePositionOnUIInverted + MouseOffset;
                 float x = Mathf.Min(pos.x, UI.screenWidth - InitialSize.x);
                 float y = Mathf.Min(pos.y, UI.screenHeight - InitialSize.y);
                 windowRect = new Rect(x, y, InitialSize.x, InitialSize.y);
             }
 
-            public override void PreOptionChosen(FloatMenuOption opt)
-            {
+            public override void PreOptionChosen(FloatMenuOption opt) {
                 parent.subMenuOptionChosen = true;
                 base.PreOptionChosen(opt);
             }
 
-            public override void PreClose()
-            {
-                foreach (FloatSubMenu sub in options.Where(o => o is FloatSubMenu))
-                {
+            public override void PreClose() {
+                foreach (FloatSubMenu sub in options.Where(o => o is FloatSubMenu)) {
                     sub.CloseSubMenu();
                 }
+                OpenMenuSet.Close(this);
             }
         }
     }
---
+++ Source/Patches.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace FloatSubMenus {
+    [StaticConstructorOnStartup]
+    [HarmonyPatch]
+    internal static class Patches {
+        static Patches() {
+            new Harmony("kathanon.FloatSubMenu").PatchAll();
+        }
+
+        private static bool replaceDist = false;
+        private static float dist;
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(FloatMenu), "UpdateBaseColor")]
+        public static void UpdateBaseColor_Pre(FloatMenu __instance) {
+            // Make sure we do not replace any values needed to calculate replacement.
+            replaceDist = false;
+            replaceDist = FloatSubMenu.ShouldReplaceDistanceFor(__instance, ref dist);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(FloatMenu), "UpdateBaseColor")]
+        public static void UpdateBaseColor_Post() {
+            replaceDist = false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GenUI), nameof(GenUI.DistFromRect))]
+        public static bool DistFromRect_Pre(ref float __result) {
+            if (replaceDist) {
+                __result = dist;
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+}
